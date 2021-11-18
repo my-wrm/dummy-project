f2d4058873cb4e5db63804fd6ff3887672019855
Thada Wangthammang <89963476+my-wrm@users.noreply.github.com>
2021-11-18 13:45:51 +0700

    Create hello-89.md (#89)
    * update
    
    * update
    
    * update
    
    * update
    
    * test
    
    * test
    
    * add event
    
    * test
    
    * test v2
    
    * auto
    
    * test
    
    * test
    
    * test
    
    * update
    
    * add reset
    
    * u
    
    * u
    
    * u
    
    * u
    
    * Create hello-89.md
    
    Co-authored-by: Thada Wangthammang <thada.wth@gmail.com>

--- create-pr.ps1
+++ create-pr.ps1
@@ -12,5 +12,5 @@ echo "hello-$branch" > .\files\hello-$branch.md
 git add .\files\hello-$branch.md
 git commit -m "Create hello-$branch.md"
 git push origin branch-$branch
-gh pr create --title "Create hello-$branch.md" --base "preview/dev" --label "[workflow] to deploy" --repo "my-wrm/dummy-project"  --body " "
+gh pr create --title "Create hello-$branch.md" --base "preview/dev" --label "to deploy" --repo "my-wrm/dummy-project"  --body " "
 git checkout main
